b7529da83ad6dff3955fafdc4eff9d1169433221
Vikarn Jha <vikarnjha91@gmail.com>
2024-07-30 19:15:23 +0530

    Assignment 1 completed successfully

---
+++ Academics/Class Assignment 1/unaryMinus.java
@@ -0,0 +1,12 @@
+public class unaryMinus {
+    public static void main(String[] args) {
+        // The unary minus operator - is used to negate a value, effectively changing its sign. For example, -5 is the negative of 5.
+
+        int number = 10;
+        int result = -number; // Apply unary minus operator
+
+        System.out.println("The result of applying unary minus to " + number + " is: " + result);
+        // Output:-
+        // The result of applying unary minus to 10 is: -10
+    }
+}
