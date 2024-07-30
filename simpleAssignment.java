15d958c182da7411f8c985eaae60a54f477c2fa7
Vikarn Jha <vikarnjha91@gmail.com>
2024-07-30 09:39:38 +0530

    add simple assignment q2

---
+++ simpleAssignment.java
@@ -0,0 +1,6 @@
+public class simpleAssignment {
+    public static void main(String[] args) {
+        System.out.println("Q2.1.1 What does the statement int x = 10 do?");        
+        System.out.println("int x = 10; creates a variable named x of type int and sets its initial value to 10.");
+    }
+}
