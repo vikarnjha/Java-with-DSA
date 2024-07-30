10687537f7f554ab204b7390e418dc126f03cd0a
Vikarn Jha <vikarnjha91@gmail.com>
2024-07-30 09:38:17 +0530

    Add arithmetic operation output

--- arithmeticOperation.java
+++ arithmeticOperation.java
@@ -1,15 +1,29 @@
+
 public class arithmeticOperation {
+
     public static void main(String[] args) {
         System.out.println("Q1.1 What is the result of 5 + 3 in Java?");
-        System.out.println(5+3);
+        System.out.println(5 + 3);
         System.out.println("Q1.2 What is the result of 10 - 4 in Java?");
-        System.out.println(10-4);
+        System.out.println(10 - 4);
         System.out.println("Q1.3 What is the result of 7 * 6 in Java?");
-        System.out.println(7*6);
+        System.out.println(7 * 6);
         System.out.println("Q1.4 What is the result of 20 / 4 in Java?");
-        System.out.println(20/4);
+        System.out.println(20 / 4);
         System.out.println("Q1.5 What is the result of 9 % 4 in Java?");
-        System.out.println(9%4);
-        
+        System.out.println(9 % 4);
+
     }
 }
+
+
+// Q1.1 What is the result of 5 + 3 in Java?
+// 8
+// Q1.2 What is the result of 10 - 4 in Java?
+// 6
+// Q1.3 What is the result of 7 * 6 in Java?
+// 42
+// Q1.4 What is the result of 20 / 4 in Java?
+// 5
+// Q1.5 What is the result of 9 % 4 in Java?
+// 1
