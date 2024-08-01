e20e225775597a40a101ee36ec973782c426f380
Vikarn Jha <vikarnjha91@gmail.com>
2024-08-01 09:52:51 +0530

    Add all mislenious

---
+++ Class-Assignment-1/unaryOperatorExample.java
@@ -0,0 +1,12 @@
+public class unaryOperatorExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = -10;
+        boolean c = true;
+        boolean d = false;
+        System.out.println(-a);
+        System.out.println(-b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
