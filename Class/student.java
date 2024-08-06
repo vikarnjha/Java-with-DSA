6efe47fbd752e3039237a5eb0900ddc919e564f1
Vikarn Jha <vikarnjha91@gmail.com>
2024-08-06 09:10:04 +0530

    add untracked files

---
+++ Class/student.java
@@ -0,0 +1,9 @@
+public class student {
+    int id;
+    String name;
+    public static void main(String[] args) {
+        student s1 = new student();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+}
