91c9d242120b03520a45ae282e5e36b8c4cc4ee1
Vikarn Jha <vikarnjha91@gmail.com>
2024-08-18 20:50:46 +0530

    add spiral pattern

---
+++ Java-with-DSA-Course/complexPattern.java
@@ -0,0 +1,20 @@
+public class complexPattern {
+    public static void main(String[] args) {
+        int n = 7;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+                    System.out.print("4 ");
+                } else if (i == 1 || i == n - 2 || j == 1 || j == n - 2) {
+                    System.out.print("3 ");
+                } else if (i == 2 || i == n - 3 || j == 2 || j == n - 3) {
+                    System.out.print("2 ");
+                } else {
+                    System.out.print("1 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
